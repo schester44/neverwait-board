ab1dd225c48385034bf45905748639b57d4f2ed9
Steven Chester <steven.chester@industryweapon.com>
2019-11-19 09:40:14 -0500

    changes for soft deleting an appointment

--- src/views/Board/queries.js
+++ src/views/Board/queries.js
@@ -1,89 +1,94 @@
-import gql from 'graphql-tag'
+import gql from "graphql-tag";
 
 export const appointmentsSubscription = gql`
-	subscription onAppointmentsChange($locationId: ID!) {
-		AppointmentsChange(locationId: $locationId) {
-			employeeId
-			isNewRecord
-			appointment {
-				id
-				status
-				duration
-				startTime
-				endTime
-				deleted
+  subscription onAppointmentsChange($locationId: ID!) {
+    AppointmentsChange(locationId: $locationId) {
+      employeeId
+      isNewRecord
+      appointment {
+        id
+        status
+        duration
+        startTime
+        endTime
 
-				employee {
-					id
-				}
-				
-				services {
-					id
-					name
-				}
+        employee {
+          id
+        }
 
-				source {
-					type
-				}
+        services {
+          id
+          name
+        }
 
-				customer {
-					firstName
-					lastName
-					profile {
-						firstName
-						lastName
-						phoneNumber
-					}
-				}
-			}
-		}
-	}
-`
+        source {
+          type
+        }
+
+        customer {
+          firstName
+          lastName
+          profile {
+            firstName
+            lastName
+            phoneNumber
+          }
+        }
+      }
+    }
+  }
+`;
 
 export const locationQuery = gql`
-	query location($startTime: DateTime!, $endTime: DateTime!) {
-		location {
-			company {
-				name
-			}
-			id
-			name
+  query location($startTime: DateTime!, $endTime: DateTime!) {
+    location {
+      company {
+        name
+      }
+      id
+      name
 
-			appointments(
-				input: { where: { status: { not: "completed" }, startTime: { gte: $startTime }, endTime: { lte: $endTime } } }
-			) {
-				id
-				status
-				duration
-				startTime
-				endTime
-				employee {
-					id
-				}
-				services {
-					id
-					name
-				}
+      appointments(
+        input: {
+          where: {
+            status: { eq: confirmed }
+            startTime: { gte: $startTime }
+            endTime: { lte: $endTime }
+          }
+        }
+      ) {
+        id
+        status
+        duration
+        startTime
+        endTime
+        employee {
+          id
+        }
+        services {
+          id
+          name
+        }
 
-				source {
-					type
-				}
+        source {
+          type
+        }
 
-				customer {
-					firstName
-					lastName
-					profile {
-						firstName
-						lastName
-						phoneNumber
-					}
-				}
-			}
+        customer {
+          firstName
+          lastName
+          profile {
+            firstName
+            lastName
+            phoneNumber
+          }
+        }
+      }
 
-			employees(input: { where: { bookingEnabled: true } }) {
-				id
-				firstName
-			}
-		}
-	}
-`
+      employees(input: { where: { bookingEnabled: true } }) {
+        id
+        firstName
+      }
+    }
+  }
+`;
